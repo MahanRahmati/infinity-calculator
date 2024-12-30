36665d35f94ee74912d0785519f82402e5d50453
Mahan <mahan.rahmati@protonmail.com>
2024-12-29 20:29:21 -0500

    feat: Remove google_fonts

--- packages/app_constants/lib/src/typography.dart
+++ packages/app_constants/lib/src/typography.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class AppTypography {
   AppTypography._();
 
   static const int _baseSize = 18;
 
-  static final TextStyle _default = GoogleFonts.inter(
+  static const TextStyle _default = TextStyle(
+    fontFamily: 'Inter',
     fontWeight: FontWeight.w400,
     color: Colors.black,
     decoration: TextDecoration.none,
@@ -15,63 +15,57 @@ class AppTypography {
 
   static TextStyle display1 = _default.copyWith(
     fontSize: _baseSize * 4,
-    fontWeight: FontWeight.w800,
+    fontWeight: FontWeight.w300,
   );
 
   static TextStyle display2 = _default.copyWith(
     fontSize: _baseSize * 3,
-    fontWeight: FontWeight.w800,
+    fontWeight: FontWeight.w300,
   );
 
   static TextStyle display3 = _default.copyWith(
     fontSize: _baseSize * 2,
-    fontWeight: FontWeight.w800,
+    fontWeight: FontWeight.w400,
   );
 
   static TextStyle title1 = _default.copyWith(
     fontSize: _baseSize * 1.81,
-    fontWeight: FontWeight.w800,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle title2 = _default.copyWith(
     fontSize: _baseSize * 1.36,
-    fontWeight: FontWeight.w800,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle title3 = _default.copyWith(
     fontSize: _baseSize * 1.36,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle title4 = _default.copyWith(
     fontSize: _baseSize * 1.18,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle heading = _default.copyWith(
-    fontWeight: FontWeight.w700,
     fontSize: _baseSize * 1,
+    fontWeight: FontWeight.w700,
   );
 
   static TextStyle body = _default.copyWith(
-    fontWeight: FontWeight.w400,
     fontSize: _baseSize * 1,
+    fontWeight: FontWeight.w400,
   );
 
   static TextStyle captionHeading = _default.copyWith(
     fontSize: _baseSize * 0.82,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeight.w500,
   );
 
   static TextStyle caption = _default.copyWith(
     fontSize: _baseSize * 0.82,
     fontWeight: FontWeight.w400,
-  );
-
-  static TextStyle monospace = GoogleFonts.robotoMono(
-    fontWeight: FontWeight.w400,
-    decoration: TextDecoration.none,
-    textBaseline: TextBaseline.alphabetic,
   );
 
   static TextStyle numeric = _default.copyWith(
